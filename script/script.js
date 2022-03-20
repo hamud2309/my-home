f7d8d7c21baf865970f6a7e411d1bd12edd9859e
hamud <hamood230987@gmail.com>
2022-03-20 16:56:25 +0000

    first commit

---
+++ script/script.js
@@ -0,0 +1,16 @@
+// Get j.s START:
+
+var hamud,user ;
+
+user=prompt ("enter you name here: ");
+user=="hamud";
+
+if(user==="hamud"){
+    alert("Welcome at your home hamud" );
+}
+else{
+    alert("Get out, you are not welcome here." );
+}
+
+
+// j.s END.
